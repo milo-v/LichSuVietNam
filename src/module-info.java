8827069c861e9e689c3c89433d4b63fbdb1d424e
Nguyen Xuan Phuoc <phuoc.nx205232@sis.hust.edu.vn>
2023-01-23 11:06:35 +0700

    Screen Upload

--- src/module-info.java
+++ src/module-info.java
@@ -5,11 +5,18 @@
  * @author milo
  *
  */
-module LichSuVietNam {
-	requires org.jsoup;
-	requires com.fasterxml.jackson.databind;
-	requires com.fasterxml.jackson.core;
-	requires com.fasterxml.jackson.annotation;
+// module LichSuVietNam {
+// 	requires org.jsoup;
+// 	requires com.fasterxml.jackson.databind;
+// 	requires com.fasterxml.jackson.core;
+// 	requires com.fasterxml.jackson.annotation;
 	
-	exports models to com.fasterxml.jackson.databind;
-}
+// 	exports models to com.fasterxml.jackson.databind;
+
+// 	requires javafx.base;
+//     requires javafx.controls;
+//     requires javafx.media;
+//     requires javafx.web;
+	
+//     exports ships;
+// }
